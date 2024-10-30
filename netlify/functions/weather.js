d53c4684b3bbc81c577f8e443e33b56c761bdfc6
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2024-10-30 14:05:06 -0400

    Update weather.js

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -3,7 +3,6 @@ const fetch = require('node-fetch');
 exports.handler = async (event) => {
   const API_KEY = process.env.OPENWEATHERMAP_API_KEY;
   const { city } = JSON.parse(event.body); // assuming you're sending city data in the request body
-  console.log("API_KEY:", API_KEY); // Only for debugging. Remove before production!
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
